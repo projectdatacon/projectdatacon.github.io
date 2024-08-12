90aaede0eb3962db11d8f4e6c484fab7358a1b37
Nery <79871298+imperiousnery@users.noreply.github.com>
2024-08-12 15:25:03 -0400

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4218818796"
+    serviceWorkerVersion: "2982290619"
   }
 });
